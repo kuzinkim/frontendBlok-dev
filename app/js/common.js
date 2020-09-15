8f754108cedac1197f8d73cc6df90a2d667ad10b
Kim <vadikpetrov53@gmail.com>
2020-09-15 19:09:21 +0300

    верстка первого экрана десктоп

--- app/js/common.js
+++ app/js/common.js
@@ -8,5 +8,36 @@ var debugTimes = false;
  * Document Ready
  */
 $(document).ready(function () {
-    
+    // подключение слайдера для первого экрана
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.slider-button.slider-button--next',
+            prevEl: '.slider-button.slider-button--prev',
+        },
+    });
+
+    // установка высоты блока с текстом 
+    function setHeight() {
+        var heightItemText = $('.slider__item').outerHeight() * 2
+        var heightItemAnimationText = $('.slider__item').outerHeight()
+        $('.main-wrapper').css("height", heightItemText + 'px')
+        $('.main-skills').css("height", heightItemAnimationText + 'px')
+    }
+
+    setHeight();
+
+    $(window).on('resize', function(){
+        setHeight();
+    })
+
+    //вфцфвф
+    $('.main').on('mousemove', function(evevt){
+        $('.circle-light').css({
+            "top": event.pageY + 'px',
+            'left':  event.pageX + 'px'
+        })
+    })
+
 });
--- app/js/libs.js
+++ app/js/libs.js
@@ -1,1 +1,2 @@
 @@include('../../node_modules/jquery/dist/jquery.js')
+@@include('../../node_modules/swiper/js/swiper.js')
